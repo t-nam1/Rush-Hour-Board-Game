06f5763a638e9aa15322e2101b54cf7c65b69e01
tnalr <96971527+tnalr@users.noreply.github.com>
2024-04-26 10:12:06 +0800

    Create car.cpp

---
+++ Components/car.cpp
@@ -0,0 +1,112 @@
+#include "Car.h"
+using namespace std;
+
+int Car::row1(string square[][BOARD_SIZE]) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
+            if (square[i][j] == color) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int Car::col1(string square[][BOARD_SIZE]) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
+            if (square[i][j] == color) {
+                return j;
+            }
+        }
+    }
+    return -1;
+}
+
+int Car::row2(string square[][BOARD_SIZE]) {
+    for (int i = BOARD_SIZE - 1; i >= 0; --i) {
+        for (int j = BOARD_SIZE - 1; j >= 0; --j) {
+            if (square[i][j] == color) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int Car::col2(string square[][BOARD_SIZE]) {
+    for (int i = BOARD_SIZE - 1; i >= 0; --i) {
+        for (int j = BOARD_SIZE - 1; j >= 0; --j) {
+            if (square[i][j] == color) {
+                return j;
+            }
+        }
+    }
+    return -1;
+}
+
+void horizontal(string square[][BOARD_SIZE], Car vehicle[8]) {
+    for (int i = 0; i < 8; ++i) {
+        if (vehicle[i].row1(square) == vehicle[i].row2(square)) {
+            vehicle[i].horizontal = "h";
+        } else { 
+            vehicle[i].horizontal = "v";
+        }
+    }
+}
+#include "Car.h"
+using namespace std;
+
+int Car::row1(string square[][BOARD_SIZE]) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
+            if (square[i][j] == color) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int Car::col1(string square[][BOARD_SIZE]) {
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
+            if (square[i][j] == color) {
+                return j;
+            }
+        }
+    }
+    return -1;
+}
+
+int Car::row2(string square[][BOARD_SIZE]) {
+    for (int i = BOARD_SIZE - 1; i >= 0; --i) {
+        for (int j = BOARD_SIZE - 1; j >= 0; --j) {
+            if (square[i][j] == color) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int Car::col2(string square[][BOARD_SIZE]) {
+    for (int i = BOARD_SIZE - 1; i >= 0; --i) {
+        for (int j = BOARD_SIZE - 1; j >= 0; --j) {
+            if (square[i][j] == color) {
+                return j;
+            }
+        }
+    }
+    return -1;
+}
+
+void horizontal(string square[][BOARD_SIZE], Car vehicle[8]) {
+    for (int i = 0; i < 8; ++i) {
+        if (vehicle[i].row1(square) == vehicle[i].row2(square)) {
+            vehicle[i].horizontal = "h";
+        } else { 
+            vehicle[i].horizontal = "v";
+        }
+    }
+}
